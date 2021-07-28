d5bb59df84c26579fdf941f73d7baf3c90a5461e
Mamba <84301026+lsoth@users.noreply.github.com>
2021-07-27 21:12:43 -0700

    updating index.js

--- index.js
+++ index.js
@@ -11,17 +11,108 @@ const db = mysql.createConnection(
     console.log("Connected to Employee Tracker")
 );
 
-const seeDepartments = () => {
+
+const viewDepartments = () => {
     db.query("SELECT * FROM departments"), (err,data)=>{
         if(err){
             console.log(err)
             db.end();
         }else{
+            console.log('im working')
             console.table(data);
             main();
         }
     }
 };
+
+// const seeEmployees = () => {
+//     db.query("SELECT * FROM employees"), (err,data)=>{
+//         if(err){
+//             console.log(err)
+//             db.end();
+//         }else{
+//             console.log(data);
+//             main();
+//         }
+//     }
+// };
+// const seeRoles = () => {
+//     db.query("SELECT * FROM roles"), (err,data)=>{
+//         if(err){
+//             console.log(err)
+//             db.end();
+//         }else{
+//             console.log(data);
+//             main();
+//         }
+//     }
+// };
+
+// const insertDepartment = () => {
+//     inquirer
+//     .prompt([
+//         {
+//             type:"input",
+//             message:"Departments name?",
+//             name:"name"
+//         }
+//     ]).then(answers => {
+//         db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name]),(err,data) => {
+//             if(err){
+//                 console.log(err);
+//                 db.destroy();
+//             } else {
+//                 console.log("department added!");
+//                 seeDepartments();
+//             }
+//         }
+
+//     })
+// };
+
+// const insertRole = () => {
+//     inquirer
+//     .prompt([
+//         {
+//             type:"input",
+//             message:"What is the name of the new role?",
+//             name:"name"
+//         }
+//     ]).then(answers => {
+//         db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name]),(err,data) => {
+//             if(err){
+//                 console.log(err);
+//                 db.destroy();
+//             } else {
+//                 console.log("department added!");
+//                 seeDepartments();
+//             }
+//         }
+
+//     })
+// };
+
+// const insertEmployee = () => {
+//     inquirer
+//     .prompt([
+//         {
+//             type:"input",
+//             message:"What is the employee's first name?",
+//             name:"first_name"
+//         }
+//     ]).then(answers => {
+//         db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name]),(err,data) => {
+//             if(err){
+//                 console.log(err);
+//                 db.destroy();
+//             } else {
+//                 console.log("department added!");
+//                 seeDepartments();
+//             }
+//         }
+
+//     })
+// };
 
 const main = () => {
     inquirer
@@ -35,7 +126,7 @@ const main = () => {
         switch (choice) {
             case "view departments":
                 // console.log('see departments');
-                seeDepartments();
+                viewDepartments();
                 break;
             default:
                 console.log('cya');
