07dd2eea2c5e74c9acd9f43c88da7fd2d623e5c0
Mamba <84301026+lsoth@users.noreply.github.com>
2021-07-27 22:54:43 -0700

    updates to index.js can add new roles now

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
 
 
 const viewDepartments = () => {
-    db.query("SELECT * FROM departments"), (err,data)=>{
+    db.query("SELECT * FROM departments", (err,data)=>{
         if(err){
             console.log(err)
             db.end();
@@ -23,74 +23,99 @@ const viewDepartments = () => {
             main();
         }
     }
+    )};
+
+const viewEmployees = () => {
+    db.query("SELECT * FROM employees", (err,data)=>{
+        if(err){
+            console.log(err)
+            db.end();
+        }else{
+            console.table(data);
+            main();
+        }
+    }
+    )};
+const viewRoles = () => {
+    db.query("SELECT roles.title AS title, salary, departments.name AS department FROM roles JOIN departments ON roles.department_id=departments.id", (err,data)=>{
+        if(err){
+            console.log(err)
+            db.end();
+        }else{
+            console.table(data);
+            main();
+        }
+    }
+    )};
+
+const insertDepartment = () => {
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            message:"Departments name?",
+            name:"name"
+        }
+    ]).then(answers => {
+        db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name],(err,data) => {
+            if(err){
+                console.log(err);
+                db.end();
+            } else {
+                console.log("department added!");
+                viewDepartments();
+            }
+        })
+
+    })
 };
 
-// const seeEmployees = () => {
-//     db.query("SELECT * FROM employees"), (err,data)=>{
-//         if(err){
-//             console.log(err)
-//             db.end();
-//         }else{
-//             console.log(data);
-//             main();
-//         }
-//     }
-// };
-// const seeRoles = () => {
-//     db.query("SELECT * FROM roles"), (err,data)=>{
-//         if(err){
-//             console.log(err)
-//             db.end();
-//         }else{
-//             console.log(data);
-//             main();
-//         }
-//     }
-// };
-
-// const insertDepartment = () => {
-//     inquirer
-//     .prompt([
-//         {
-//             type:"input",
-//             message:"Departments name?",
-//             name:"name"
-//         }
-//     ]).then(answers => {
-//         db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name]),(err,data) => {
-//             if(err){
-//                 console.log(err);
-//                 db.destroy();
-//             } else {
-//                 console.log("department added!");
-//                 seeDepartments();
-//             }
-//         }
-
-//     })
-// };
-
-// const insertRole = () => {
-//     inquirer
-//     .prompt([
-//         {
-//             type:"input",
-//             message:"What is the name of the new role?",
-//             name:"name"
-//         }
-//     ]).then(answers => {
-//         db.query(`INSERT INTO departments (name) VALUES(?)`,[answers.name]),(err,data) => {
-//             if(err){
-//                 console.log(err);
-//                 db.destroy();
-//             } else {
-//                 console.log("department added!");
-//                 seeDepartments();
-//             }
-//         }
-
-//     })
-// };
+const insertRole = () => {
+    db.query("SELECT * FROM departments", (err,data)=>{
+        if(err){
+            console.log(err)
+            db.end();
+        }else {
+        const inqDept = data.map( department => {
+            return {
+            name: department.name,
+            value:department.id
+            }
+        })
+    inquirer
+    .prompt([
+        {
+            type:"list",
+            message:"What is the department of the new role?",
+            choices:inqDept,
+            name:"department_id"
+        },
+        {
+            type:"input",
+            message:"What is the title of the new role?",
+            name:"title"
+        },
+        {
+            type:"input",
+            message:"What is the salary of the new role?",
+            name:"salary"
+        }
+    ]).then(answers => {
+        db.query(`INSERT INTO roles (title,salary,department_id) VALUES(?,?,?)`,
+        [answers.title,answers.salary,answers.department_id],(err,data) => {
+            if(err){
+                console.log(err);
+                db.destroy();
+            } else {
+                console.log("role added!");
+                viewRoles();
+            }
+        }
+        )
+    });
+}
+})
+};
 
 // const insertEmployee = () => {
 //     inquirer
@@ -118,15 +143,29 @@ const main = () => {
     inquirer
     .prompt({
         type:"list",
-        choices: ["view departments", "quit"],
+        choices: ["view departments", "view roles", "view employees", "add new department", "add new role", "quit"],
         message: "what do you want to do?",
         name: "choice"
     })
     .then(({choice}) => {
         switch (choice) {
             case "view departments":
-                // console.log('see departments');
                 viewDepartments();
+                break;
+            case "add new department":
+                insertDepartment();
+                break;
+            case "view roles":
+                viewRoles();
+                break;
+            case "add new role":
+                insertRole();
+                break;
+            case "view employees":
+                viewEmployees();
+                break;
+            case "add new employees":
+                insertEmployee();
                 break;
             default:
                 console.log('cya');
